67863a47a274586d4b580996ef34f02c560b8483
朱洋洋 <zhuyy@2345.com>
2019-12-13 11:46:11 +0800

    简述:我的第一个包 代码检测: 产品人员: QSID:

---
+++ demo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhuyy
+ * Date: 2019/12/13
+ * Time: 11:37
+ */
+
+require_once "./vendor/autoload.php";
+
+use Abstarfish\Starfish\Tools;
+
+Tools::Hello();
---
+++ src/Abstarfish/Starfish/Tools.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhuyy
+ * Date: 2019/12/13
+ * Time: 11:28
+ */
+namespace Abstarfish\Starfish;
+
+class Tools
+{
+    /**
+     * Tools constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public static function Hello()
+    {
+        echo "Hello World";
+    }
+}
